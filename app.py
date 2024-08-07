56dd8942c2f81ddcd8f09f308a8c375145255069
Adler1312 <Altrim.Muharemi@hotmail.com>
2024-08-07 02:05:58 +0200

    App.py ist fertig

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ import random
 import os
 
 app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/coin_toss', methods=['GET'])
+def coin_toss():
+    outcome = random.choice(["Kopf", "Zahl"])
+    return jsonify(result=outcome)
+
+if __name__ == '__main__':
+    app.run(debug=True)
